ac07246037ff6ad22dc9d191ba307f5fffd2707b
salah atwa <salahsayedatwa@gmail.com>
2022-09-27 13:34:39 +0300

    Update TestController.java

--- src/main/java/com/altafjava/controller/TestController.java
+++ src/main/java/com/altafjava/controller/TestController.java
@@ -8,6 +8,6 @@ public class TestController {
 
   @GetMapping("/test")
   public String test() {
-    return "This is TestController";
+    return "This is TestController testing startup shell script ................................ _^_6";
   }
 }
